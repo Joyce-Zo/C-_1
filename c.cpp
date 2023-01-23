5bd4e299d58c1af6ef0a4b4c7334d8f7cdb3c589
Joyce-Zo <109672607+Joyce-Zo@users.noreply.github.com>
2023-01-23 21:54:30 +0800

    Add files via upload

---
+++ c.cpp
@@ -0,0 +1,121 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <iostream>
+#include <string>
+using namespace std;
+
+#define day 7
+int main()
+{
+	// 1.1
+	//cout << "一周共有" << day << "天" << endl;
+	//cout << "hello world!" <<endl;
+	//			endl - 换行
+	//int num = 100;
+	//cout << "num= " << num << endl;
+	//		 打印的数据  
+
+	//const int month = 12;
+	//month = 13;
+	//cout << "一年共有" << month << "个月";
+	// 1.5
+	//int 你好 = 5;
+	//cout << 你好;
+
+	// 2.1
+	/*short num1 = 4;
+	int num2 = 5;
+	long num3 = 6;
+	long long num4 = 7;
+
+	cout << sizeof(num1) <<sizeof(short)<< endl;
+	cout << sizeof(num2) <<sizeof(int)<< endl;
+	cout << sizeof(num3) <<sizeof(long)<< endl;
+	cout << sizeof(num4) <<sizeof(long long);*/
+
+	//2.2
+	//float f1 = 6.88f;
+	////cout << f1 << endl;
+	//cout << sizeof(f1) << endl;
+	//double d1 = 6.888888888888;
+	////cout << d1;
+	//cout << sizeof(d1);
+	//float f1 = 6e4;
+	//cout << f1 << endl;
+	//float f2 = 6e-4;
+	//cout << f2 << endl;
+	//float f3 = 6e5;
+	//cout << f3;
+
+	// 2.4
+	/*char ch = 'B';
+	cout << ch << endl;
+	cout << sizeof(ch) << endl;
+	cout << int(ch);*/
+
+	// 2.5
+	//cout << "hello\n" << "world" << endl;
+	//cout << "\\";
+	/*cout << "h\tworld" << endl;
+	cout << "he\tworld" << endl;
+	cout << "hel\tworld" << endl;
+	cout << "hell\tworld" << endl;
+	cout << "hello\tworld" << endl;
+
+	cout << "h world" << endl;
+	cout << "he world" << endl;
+	cout << "hel world" << endl;
+	cout << "hell world" << endl;
+	cout << "hello world" << endl;*/
+
+	// 2.6
+	/*char str1[] = "hello baby";
+	cout << str1 << endl;
+	cout << sizeof(str1) << endl;
+
+	string str2 = "hello baby";
+	cout << str2 << endl;
+	cout << sizeof(str2);*/
+
+	// 2.7
+	//bool flag = false;
+	////cout << flag << endl;
+	//while (!flag)
+	//{
+	//	cin >> flag;
+	//	cout << flag;
+	//}
+
+	/*flag = false;
+	cout << flag << endl;*/
+
+	//cout << sizeof(flag);
+
+	// 2.8
+	/*int num1 = 0;
+	cout << "请输入数据:";
+	cin >> num1;
+	cout << num1 << endl;
+
+	float num2;
+	cout << "请输入数据:";
+	cin >> num2;
+	cout << num2 << endl;
+
+	char num3;
+	cout << "请输入数据:";
+	cin >> num3;
+	cout << num3 << endl;
+
+	string num4;
+	cout << "请输入数据:";
+	cin >> num4;
+	cout << num4 << endl;
+
+	bool num5;
+	cout << "请输入数据:";
+	cin >> num5;
+	cout << num5 << endl;*/
+
+
+	return 0;
+}
